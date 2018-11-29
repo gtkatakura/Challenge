3f5090c8521ec460599a170acb86f41076b9051f
gtkatakura <gt.katakura@gmail.com>
2018-11-29 21:52:05 -0200

    feat(products): infinite flat list

---
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+import { withApollo } from 'react-apollo'
+import { gql } from 'apollo-boost'
+import { ActivityIndicator, FlatList } from 'react-native'
+import {
+  Avatar, SearchBar, ListItem,
+} from 'react-native-elements'
+import _ from 'lodash/fp'
+
+import { withAuthentication } from '../../services'
+
+const ProductCollectionQuery = gql`
+  query ProductCollectionQuery($after: ID) {
+    products(after: $after) {
+      payload {
+        id
+        name
+      }
+      hasMore
+    }
+  }
+`
+
+const firstLetters = text => text.split(' ').map(word => word[0]).join('')
+
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Home',
+  }
+
+  state = {
+    products: {
+      payload: [],
+    },
+    loadingMore: false,
+  }
+
+  logout = async () => {
+    await this.props.authentication.reset()
+    this.props.navigation.navigate('Auth')
+  }
+
+  loadMore = async () => {
+    if (!this.state.products.hasMore) {
+      return
+    }
+
+    this.setState({
+      loadingMore: true,
+    })
+
+    const lastProduct = _.last(this.state.products.payload)
+
+    const { data: { products } } = await this.props.client.query({
+      query: ProductCollectionQuery,
+      variables: {
+        after: lastProduct.id,
+      },
+    })
+
+    this.setState(prevState => ({
+      products: {
+        ...products,
+        payload: prevState.products.payload.concat(products.payload),
+      },
+    }))
+
+    this.setState({
+      loadingMore: false,
+    })
+  }
+
+  async componentDidMount() {
+    const { data: { products } } = await this.props.client.query({
+      query: ProductCollectionQuery,
+    })
+
+    this.setState({
+      products,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <SearchBar placeholder="Type here..." lightTheme />
+        <FlatList
+          data={this.state.products.payload}
+          keyExtractor={_.get('id')}
+          renderItem={({ item: product }) => (
+            <ListItem
+              key={product.id}
+              avatar={(
+                <Avatar
+                  title={firstLetters(product.name)}
+                  rounded
+                />
+              )}
+              title={product.name}
+            />
+          )}
+          onEndReached={this.loadMore}
+          onEndReachedThreshold={0.5}
+          ListFooterComponent={() => this.state.loadingMore && (
+            <ActivityIndicator size="large" animating />
+          )}
+        />
+      </>
+    )
+  }
+}
+
+export default withApollo(withAuthentication(HomeScreen))
