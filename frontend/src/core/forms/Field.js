099ce531ab1354aa63c0c178b42769a8f1c2a9ae
gtkatakura <gt.katakura@gmail.com>
2018-12-11 22:04:18 -0200

    fix(fields): trim value only in blur

--- frontend/src/core/forms/Field.js
+++ frontend/src/core/forms/Field.js
@@ -11,19 +11,28 @@ class Field extends Component {
     name: PropTypes.string.isRequired,
   }
 
-  handleChange = (value) => {
-    this.context.update(this.props.name, value.trim())
-  }
+  getValue = () => this.context.values[this.props.name]
+
+  update = value => this.context.update(this.props.name, value)
+
+  handleChange = value => this.update(value)
+
+  handleBlur = () => this.update(this.getValue().trim())
 
   render() {
     const { name, label, ...props } = this.props
-    const value = this.context.values[name]
+    const value = this.getValue()
     const error = this.context.errors[name]
 
     return (
       <>
         <FormLabel>{label}</FormLabel>
-        <FormInput onChangeText={this.handleChange} value={value && value.toString()} {...props} />
+        <FormInput
+          onChangeText={this.handleChange}
+          onBlur={this.handleBlur}
+          value={value && value.toString()}
+          {...props}
+        />
         {error && (
           <FormValidationMessage>{error.toString()}</FormValidationMessage>
         )}
